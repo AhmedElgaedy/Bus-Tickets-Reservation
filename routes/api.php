d8e23f16e4611e39c289e82f59123e9559fea938
AhmedElgeady <a.mostafa199964@gmail.com>
2023-09-16 02:47:32 +0300

    set reservation

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('available-seats/trip/{trip}', [ReservationController::class, 'getAvailableSeats']);
+Route::post('reservation/trip/{trip}', [ReservationController::class, 'reservationSeat']);
